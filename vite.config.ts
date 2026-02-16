2fb788a2c5e1277b50f92dae9fcffb4bd0613445
dmgproductionsoficial <dmgproductionsoficial@gmail.com>
2026-02-16 01:44:42 +0000

    SYNC

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,12 @@
-
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 import javascriptObfuscator from 'vite-plugin-javascript-obfuscator';
 
 export default defineConfig({
   plugins: [
-    react(),
     javascriptObfuscator({
       options: {
         compact: true,
-        controlFlowFlattening: true,
-        controlFlowFlatteningThreshold: 0.75,
-        deadCodeInjection: true,
-        deadCodeInjectionThreshold: 0.4,
-        debugProtection: false,
-        debugProtectionInterval: 0,
-        disableConsoleOutput: true,
-        identifierNamesGenerator: 'hexadecimal',
-        log: false,
-        numbersToExpressions: true,
-        renameGlobals: false,
-        selfDefending: true,
-        shuffleStringArray: true,
-        splitStrings: true,
-        splitStringsChunkLength: 10,
-        stringArray: true,
-        stringArrayEncoding: ['base64'],
-        stringArrayThreshold: 0.75,
-        transformObjectKeys: true,
-        unicodeEscapeSequence: false
+        stringArray: true
       }
     })
   ]
